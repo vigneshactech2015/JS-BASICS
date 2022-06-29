347774071ab1c30870dab8946b4a9debd3013804
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2022-06-29 10:03:45 +0530

    Update closure.js

--- JS/closure.js
+++ JS/closure.js
@@ -22,3 +22,14 @@ function c(){
     }
 }
 c();
+
+function x(){
+    for (var i=1;i<=5;i++){
+        function close(x){
+        setTimeout(function(){
+            console.log(x);
+        },x*1000);}
+        close(i);
+    }
+}
+x();
