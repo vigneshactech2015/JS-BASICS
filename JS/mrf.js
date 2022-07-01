d76324a247abda94c0edf26b586aa3ba6703de58
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2022-07-01 14:16:19 +0530

    Update mrf.js

--- JS/mrf.js
+++ JS/mrf.js
@@ -87,3 +87,21 @@ let usd=data.filter((ele)=>{
     }
 }).map((ele)=>ele.name);
 console.log(usd);
+
+
+const array = ['🐑', 1, 2, '🐑', '🐑', 3];
+
+// 1: "Set"
+[...new Set(array)];
+
+// 2: "Filter"
+array.filter((item, index) => array.indexOf(item) === index);
+
+// 3: "Reduce"
+array.reduce(
+  (unique, item) => (unique.includes(item) ? unique : [...unique, item]),
+  [],
+);
+
+// RESULT:
+// ['🐑', 1, 2, 3];
